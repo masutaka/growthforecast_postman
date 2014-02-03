a83b1f58603260873c37134fb947038064765892
Takashi Masuda <masutaka.net@gmail.com>
2014-02-03 22:25:16 +0900

    Add comments of API source

--- lib/feedly.rb
+++ lib/feedly.rb
@@ -5,6 +5,7 @@ require_relative 'social_service'
 
 class Feedly < SocialService
   def subscribers
+    # http://developer.feedly.com/v3/feeds/#get-the-metadata-about-a-specific-feed
     api_uri = 'http://cloud.feedly.com/v3/feeds/feed%2F' + URI.escape(@blog_rss, ':/')
     JSON.parse(Net::HTTP.get(URI.parse(api_uri)))['subscribers']
   end
--- lib/livedoor_reader.rb
+++ lib/livedoor_reader.rb
@@ -4,6 +4,7 @@ require_relative 'social_service'
 
 class LivedoorReader < SocialService
   def subscribers
+    # http://seesaawiki.jp/w/staff_reader/d/Count%20API
     api_uri = 'http://rpc.reader.livedoor.com/count?feedlink=' + @blog_rss
     Net::HTTP.get(URI.parse api_uri).to_i
   end
